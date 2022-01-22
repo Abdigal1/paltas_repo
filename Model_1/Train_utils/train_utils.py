b5c1afd7bed490e95e6e9af1dafa8ba553126905
guillermo <gmorenoc@uni.pe>
2022-01-22 01:11:42 -0500

    checkpoint

--- Model_1/Train_utils/train_utils.py
+++ Model_1/Train_utils/train_utils.py
@@ -2,6 +2,27 @@ from tqdm import tqdm
 import numpy as np
 import torch
 import os
+import pickle
+
+def save_dict(dict,path):
+    file=open(path,'wb')
+    pickle.dump(dict,file)
+    file.close()
+
+def load_dict(path):
+    file=open(path,'rb')
+    dict=pickle.load(file,'rb')
+    file.close()
+    return dict
+
+def security_checkpoint(current_epoch,total_epoch,model,optimizer,loss,PATH):
+    torch.save({
+        'current_epoch':current_epoch,
+        'total_epoch':total_epoch,
+        'model_state_dict':model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss':loss
+    },PATH)
 
 def train(model,optimizer,dataloader,use_cuda,loss_function,in_device=None):
     loss_d=[]
@@ -64,7 +85,7 @@ def test(model,dataloader,use_cuda,loss_function,in_device=None):
         kld_d.append(kld.item())
     return loss_d,bce_d,kld_d
 
-def train_test(model,optimizer,train_set,test_set,batch_size,use_cuda,loss_function,epochs,data_train_dir,in_device=None):
+def train_test(model,optimizer,train_set,test_set,batch_size,use_cuda,loss_function,epochs,data_train_dir,in_device=None,checkpoint_epoch=0):
     epoch_loss={}
     epoch_bce={}
     epoch_kld={}
@@ -76,28 +97,39 @@ def train_test(model,optimizer,train_set,test_set,batch_size,use_cuda,loss_funct
     epoch_loss_test=[]
     epoch_bce_test=[]
     epoch_kld_test=[]
+
+    #Is file already exists charge ------------------------------------------------------------------------------------------------------------------------
+    if "loss_results.npy" in os.listdir(data_train_dir):
+        epoch_bce_train=np.load(os.path.join(data_train_dir,'bce_results.npy'),allow_pickle=True).tolist()['train']
+        epoch_bce_test=np.load(os.path.join(data_train_dir,'bce_results.npy'),allow_pickle=True).tolist()['valid']
+
+        epoch_kld_train=np.load(os.path.join(data_train_dir,'kld_results.npy'),allow_pickle=True).tolist()['train']
+        epoch_kld_test=np.load(os.path.join(data_train_dir,'kld_results.npy'),allow_pickle=True).tolist()['valid']
+
+        epoch_loss_train=np.load(os.path.join(data_train_dir,'loss_results.npy'),allow_pickle=True).tolist()['train']
+        epoch_loss_test=np.load(os.path.join(data_train_dir,'loss_results.npy'),allow_pickle=True).tolist()['valid']
+
     
     best_result=0
 
-    for epoch in tqdm(range(epochs),desc="Epoch"):
+    for epoch in tqdm(range(checkpoint_epoch,epochs),desc="Epoch"):
 
-        drop=False
-        if len(train_set)%batch_size==1 or len(test_set)%batch_size==1:
-            drop=True
+        drop_train=False
+        drop_test=False
+        if len(train_set)%batch_size==1:
+            drop_train=True
+        
+        if len(test_set)%batch_size==1:
+            drop_test=True
 
-<<<<<<< HEAD
-        dataloader_train=torch.utils.data.DataLoader(train_set,batch_size=batch_size,shuffle=True,num_workers=6,drop_last=drop)
-        dataloader_test=torch.utils.data.DataLoader(test_set,batch_size=batch_size,shuffle=True,num_workers=6,drop_last=drop)
-=======
-        dataloader_train=torch.utils.data.DataLoader(train_set,batch_size=batch_size,shuffle=True,num_workers=0,drop_last=drop)
-        dataloader_test=torch.utils.data.DataLoader(test_set,batch_size=batch_size,shuffle=True,num_workers=0,drop_last=drop)
->>>>>>> d4a9669d5f2b9e6402c995b2e38a92166534a97b
+        dataloader_train=torch.utils.data.DataLoader(train_set,batch_size=batch_size,shuffle=True,num_workers=0,drop_last=drop_train)
+        dataloader_test=torch.utils.data.DataLoader(test_set,batch_size=batch_size,shuffle=True,num_workers=0,drop_last=drop_test)
 
-        loss_d,bce_d,kld_d=train(model,optimizer,dataloader_train,use_cuda,loss_function,in_device)
+        loss_tr,bce_tr,kld_tr=train(model,optimizer,dataloader_train,use_cuda,loss_function,in_device)
     
-        epoch_loss_train.append(np.mean(np.array(loss_d)))
-        epoch_bce_train.append(np.mean(np.array(bce_d)))
-        epoch_kld_train.append(np.mean(np.array(kld_d)))
+        epoch_loss_train.append(np.mean(np.array(loss_tr)))
+        epoch_bce_train.append(np.mean(np.array(bce_tr)))
+        epoch_kld_train.append(np.mean(np.array(kld_tr)))
     
         loss_d,bce_d,kld_d=test(model,dataloader_test,use_cuda,loss_function,in_device)
         #loss_d,bce_d,kld_d=test(model,dataloader_train,use_cuda,loss_function)
@@ -134,6 +166,15 @@ def train_test(model,optimizer,train_set,test_set,batch_size,use_cuda,loss_funct
         np.save(os.path.join(data_train_dir,"bce_results"+'.npy'),epoch_bce)
         np.save(os.path.join(data_train_dir,"kld_results"+'.npy'),epoch_kld)
 
+        #SAVE CHECKPOINT ------------------------------------------------------------------------------------------------------------------------------------------------
+        security_checkpoint(current_epoch=epoch,
+                            total_epoch=epochs,
+                            model=model,
+                            optimizer=optimizer,
+                            loss=loss_tr,
+                            PATH=os.path.join(data_train_dir,"checkpoint.pt")
+                            )
+
     
     return epoch_loss_train,epoch_bce_train,epoch_kld_train,epoch_loss_test,epoch_bce_test,epoch_kld_test,best_model
 
@@ -152,24 +193,55 @@ def K_fold_train(model,
     fold_kld={}
 
     #Shuffle data
-    train_s=int((len(dataset))*0.7)
+    train_s=int((len(dataset))*0.8)
     test_s=int(len(dataset)-train_s)
     print("train len")
     print(train_s)
     print("test len")
-    print(test_s)    
-    train_set, test_set = torch.utils.data.random_split(dataset, [train_s, test_s])
+    print(test_s)
 
-    drop=False
-    if train_s%batch_size==1 or test_s%batch_size==1:
-        drop=True
+    #LOAD INDEXES IS ALREADY EXISTS ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    if "data_split.pkl" in os.listdir(data_train_dir):
+        dict=load_dict(os.path.join(data_train_dir,"data_split.pkl"))
+        train_index=dict["train_index"]
+        test_index=dict["test_index"]
+    else:
+        train_index,test_index=torch.utils.data.random_split(range(len(dataset)),[train_s, test_s])
+        dataset_split_index={
+            "train_index":train_index,
+            "test_index":test_index
+        }
+        save_dict(dataset_split_index,os.path.join(data_train_dir,"data_split.pkl"))
+
+    #train_set, test_set = torch.utils.data.random_split(dataset, [train_s, test_s])
+    train_set = torch.utils.data.Subset(dataset, train_index)
+    test_set = torch.utils.data.Subset(dataset, test_index)
+
+    drop_train=False
+    drop_test=False
+    if train_s%batch_size==1:
+        drop_train=True
+    
+    if test_s%batch_size==1:
+        drop_test=True
 
     for fold in tqdm(range(folds),desc="folds"):
         train_set, test_set = torch.utils.data.random_split(dataset, [train_s, test_s])
 
         ed=model
         #optimizer
-        optimizer=torch.optim.Adam(ed.parameters(),lr=1e-3)
+        optimizer=torch.optim.Adam(model.parameters(),lr=1e-3)
+
+        #LOAD OPTIMIZER, MODEL, CURRENT EPOCH AND NUMBER OF EPOCHS FROM CHECKPOINT ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        if "checkpoint.pt" in os.listdir(data_train_dir):
+            checkpoint=torch.load(os.path.join(data_train_dir,"checkpoint.pt"))
+            model.load_state_dict(checkpoint['model_state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            checkpoint_epoch=checkpoint["current_epoch"]
+            epochs=checkpoint["total_epoch"]
+        else:
+            checkpoint_epoch=0
+
 
         #Epochs
         epoch_loss_train,epoch_bce_train,epoch_kld_train,epoch_loss_test,epoch_bce_test,epoch_kld_test,best_model=train_test(
@@ -182,7 +254,8 @@ def K_fold_train(model,
             loss_function=loss_fn,
             epochs=epochs,
             data_train_dir=data_train_dir,
-            in_device=in_device
+            in_device=in_device,
+            checkpoint_epoch=checkpoint_epoch
         )
 
         fold_loss[fold]={"train":epoch_loss_train,
@@ -203,6 +276,6 @@ def K_fold_train(model,
                     fold=fold
                     ))
 
-    np.save(os.path.join(data_train_dir,"loss_results"+'.npy'),fold_loss)
-    np.save(os.path.join(data_train_dir,"bce_results"+'.npy'),fold_bce)
-    np.save(os.path.join(data_train_dir,"kld_results"+'.npy'),fold_kld)
+    np.save(os.path.join(data_train_dir,"fold_loss_results"+'.npy'),fold_loss)
+    np.save(os.path.join(data_train_dir,"fold_bce_results"+'.npy'),fold_bce)
+    np.save(os.path.join(data_train_dir,"fold_kld_results"+'.npy'),fold_kld)
