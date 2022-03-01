1abc9913d1f35d4ae6a014782b54eafcc79f69b3
grmc1999 <gmorenoc@uni.pe>
2022-02-28 23:43:46 -0500

    generate m5

--- Model_5/results/Results_processing/generate_VAE.py
+++ Model_5/results/Results_processing/generate_VAE.py
@@ -3,11 +3,17 @@ import os
 import sys
 
 sys.path.append(os.path.join("..",".."))
+sys.path.append(os.path.join("..","..","..",'Data_prep'))
+sys.path.append(os.path.join("..","..","..",'DL_utils'))
+sys.path.append(os.path.join("..","..",'..','ndvi'))
+
+sys.path.append(os.path.join("..",".."))
 
 import torch
+from torch import nn
 from Utils import *
 
-from B_VAE.VAE_v2 import b_encodeco
+from Net.VAE_meta import b_encodeco
 
 from torchvision import transforms
 from Custom_dataloader import *
@@ -16,39 +22,52 @@ from Transforms import phantom_segmentation_
 from Transforms import multi_image_resize
 from Transforms import multi_ToTensor
 from Transforms import output_transform
-from Transforms import rgb_normalize
+from Transforms import *
 
 
 
 def main():
-    res_dir=os.path.join("..",'VAE_v2_4')
+    res_dir=os.path.join("..",'Meta_VAE_A2_2')
     model_state=torch.load(os.path.join(res_dir,'best0.pt'))
 
     #DB="/run/user/1000/gvfs/afp-volume:host=MyCloudPR4100.local,user=aorus_1,volume=Paltas_DataBase/Data_Base_v2"
-    DB="/home/liiarpi-01/proyectopaltas/Local_data_base/Data_Base_v2"
-    meta_dir="/run/user/1000/gvfs/afp-volume:host=MyCloudPR4100.local,user=aorus_1,volume=Paltas_DataBase/metadata_VAE_v4"
+    DB="/home/liarpi2/proyectopaltas/Local_data_base/Data_Base_v2"
+    meta_dir="/home/liarpi2/proyectopaltas/Local_data_base/metadata_Meta_VAE_A2_2"
 
-    model=b_encodeco(image_dim=int(200),
+    device="cuda"
+
+    d_tt=transforms.Compose([
+        phantom_segmentation(False,non_uniform_input=True),
+        rgb_normalize(ImType=['PhantomRGB']),
+        #hue_transform(),
+        multi_image_resize(ImType=['PhantomRGB'],size=(400,400)),
+        pos_fly_transform(),
+        concatenate_non_uniform_transform(),
+        multi_ToTensor(ImType=['PhantomRGB']),
+        select_out_transform(selected=['PhantomRGB','Non_uniform_input','Place','Date','landmarks'])### ----------------------------------------
+        ])
+
+    model=b_encodeco(
+                image_dim=int(400),
                  image_channels=3,
-                 repr_sizes=[10,20,40],
-                 layer_sizes=[],
-                 latent_space_size=50,
-                 conv_kernel_size=15,
+                 non_uniform_dim=30,
+                 repr_sizes=[4,8,16],
+                 pre_layer_sizes=[300,200],
+                 layer_sizes=[100,50],
+                 pre_output=200,
+                 latent_space_size=12,
+                 conv_kernel_size=5,
+                 activators=[nn.Sigmoid(),nn.LeakyReLU(),nn.LeakyReLU(),nn.LeakyReLU()],
                  conv_pooling=False,
                  conv_batch_norm=True,
                  NN_batch_norm=True,
                  stride=2,
-                device='cpu')
+                device=device)
+    
 
     model.load_state_dict(model_state)
 
-    d_tt=transforms.Compose([
-        phantom_segmentation_(False),
-        rgb_normalize(ImType=['PhantomRGB']),
-        multi_image_resize(ImType=['PhantomRGB'],size=(200,200)),
-        multi_ToTensor(ImType=['PhantomRGB']),
-        select_out_transform(selected=['SenteraNDVI','Place','Date','landmarks'])### ----------------------------------------
-        ])
+    model.to(device)
 
     datab=Dataset_direct(root_dir=DB,ImType=['PhantomRGB'],Intersec=False,transform=d_tt)
 
@@ -62,7 +81,7 @@ def main():
                                     model=model,
                                     batch_size=20,
                                     num_workers=6,
-                                    args=['SenteraNDVI'],
+                                    args=['PhantomRGB','Non_uniform_input'],
                                     device_in='cuda')
 
     
