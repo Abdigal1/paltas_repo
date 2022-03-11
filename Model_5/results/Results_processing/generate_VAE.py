4808afcd16279c81e3044561e9797656bfce5fbb
Abdigal1 <acamargob@uni.pe>
2022-03-10 21:02:38 -0500

    10-03-9-02-pm

--- Model_5/results/Results_processing/generate_VAE.py
+++ Model_5/results/Results_processing/generate_VAE.py
@@ -27,49 +27,50 @@ from Transforms import *
 
 
 def main():
-    res_dir=os.path.join("..",'Meta_VAE_A2_2')
+    res_dir=os.path.join("..",'metaVAE_L_2')
     model_state=torch.load(os.path.join(res_dir,'best0.pt'))
 
     #DB="/run/user/1000/gvfs/afp-volume:host=MyCloudPR4100.local,user=aorus_1,volume=Paltas_DataBase/Data_Base_v2"
-    DB="/home/liarpi2/proyectopaltas/Local_data_base/Data_Base_v2"
-    meta_dir="/home/liarpi2/proyectopaltas/Local_data_base/metadata_Meta_VAE_A2_2"
+    DB="/home/lambda/paltas/Local_data_base/Data_Base_v2"
+    meta_dir="/home/lambda/paltas/Local_data_base/metadata_metaVAE_L_2"
 
     device="cuda"
 
     d_tt=transforms.Compose([
-        phantom_segmentation(False,non_uniform_input=True),
-        rgb_normalize(ImType=['PhantomRGB']),
-        #hue_transform(),
-        multi_image_resize(ImType=['PhantomRGB'],size=(400,400)),
-        pos_fly_transform(),
+        trans_registration(),
+        multi_image_resize(ImType=['SenteraRGB','SenteraNIR'],size=(512,512)),
+        stack_multiespectral(),
+        pos_fly_transform(ImType=["SenteraRGB"]),
         concatenate_non_uniform_transform(),
-        multi_ToTensor(ImType=['PhantomRGB']),
-        select_out_transform(selected=['PhantomRGB','Non_uniform_input','Place','Date','landmarks'])### ----------------------------------------
+        multi_ToTensor(ImType=['SenteraRGBNIR']),
+        select_out_transform(selected=['SenteraRGBNIR','Non_uniform_input','Place','Date','landmarks'])### ----------------------------------------
         ])
 
-    model=b_encodeco(
-                image_dim=int(400),
-                 image_channels=3,
-                 non_uniform_dim=30,
-                 repr_sizes=[4,8,16],
+    model=b_encodeco(image_dim=int(512),
+                 image_channels=5,
+                 non_uniform_dim=33,
                  pre_layer_sizes=[300,200],
-                 layer_sizes=[100,50],
-                 pre_output=200,
-                 latent_space_size=12,
-                 conv_kernel_size=5,
-                 activators=[nn.Sigmoid(),nn.LeakyReLU(),nn.LeakyReLU(),nn.LeakyReLU()],
+                 pre_output=500,
+                 repr_sizes=[8,16,32,64,128],
+                 layer_sizes=[100,80,50],
+                 latent_space_size=40,
+                 conv_kernel_size=7,
+                 activators=[nn.Sigmoid(),nn.LeakyReLU(),nn.LeakyReLU(),nn.LeakyReLU(),nn.LeakyReLU()],
                  conv_pooling=False,
                  conv_batch_norm=True,
                  NN_batch_norm=True,
                  stride=2,
-                device=device)
+                device="cuda",
+                 Multi_GPU=True,
+                 in_device="cuda:0")
+                
     
 
     model.load_state_dict(model_state)
 
     model.to(device)
 
-    datab=Dataset_direct(root_dir=DB,ImType=['PhantomRGB'],Intersec=False,transform=d_tt)
+    datab=Dataset_direct(root_dir=DB,ImType=['SenteraRGB','SenteraNIR','SenteraMASK'],Intersec=False,transform=d_tt)
 
     #gen_metadata_from_model(data_base=datab,
     #                        out_meta_dir=meta_dir,
@@ -79,9 +80,9 @@ def main():
     parallel_gen_metadata_from_VAE(data_base=datab,
                                     out_meta_dir=meta_dir,
                                     model=model,
-                                    batch_size=20,
-                                    num_workers=6,
-                                    args=['PhantomRGB','Non_uniform_input'],
+                                    batch_size=40,
+                                    num_workers=9,
+                                    args=['SenteraRGBNIR','Non_uniform_input'],
                                     device_in='cuda')
 
     
